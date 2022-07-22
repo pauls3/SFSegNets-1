d3db7753a43d3dc870193a79a36b785839ee3589
pauls3 <paulstanik3@gmail.com>
2022-07-22 08:39:50 -0700

    updating test script for new dir

--- scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time_stylegan2.sh
+++ scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time_stylegan2.sh
@@ -3,6 +3,6 @@
 # echo "Saving Results :" ${2}
 python3 demo_floder.py \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
-    --save_dir logs/railsem19_pretrained_cityscapes_0/stylegan2-ada-pytorch_veg/2\
+    --save_dir logs/railsem19_pretrained_cityscapes_0/stylegan2-ada-pytorch_veg/3\
     --snapshot pretrained_models/railsem19_cityscapes_mapillary_epoch_252_mean-iou_0.68545.pth \
-    --demo_folder input_data/stylegan2-ada-pytorch/veg/2
+    --demo_folder input_data/stylegan2-ada-pytorch/veg/3
