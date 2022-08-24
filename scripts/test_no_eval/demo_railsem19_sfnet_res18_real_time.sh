5e2c3990f420c73ffe335642379333b6429d2aa9
pauls3 <paulstanik3@gmail.com>
2022-08-23 23:30:32 -0700

    updated test scripts for rs19 class color testing

--- scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
+++ scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 # echo "Running inference on" ${1}
 # echo "Saving Results :" ${2}
+# --demo_folder /home/stanik/rtis_lab/data/RailSem19/custom_split/test_images
 python3 demo_floder.py \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
-    --save_dir logs/railsem19_pretrained_cityscapes_0/no_eval_test_0 \
+    --save_dir logs/railsem19_pretrained_cityscapes_0/no_eval_test_1 \
     --snapshot pretrained_models/railsem19_cityscapes_mapillary_epoch_252_mean-iou_0.68545.pth \
     --demo_folder /home/stanik/rtis_lab/data/RailSem19/custom_split/test_images
