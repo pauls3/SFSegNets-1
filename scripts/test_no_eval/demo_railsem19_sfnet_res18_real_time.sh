7df33712a07b33840f0cb9e0d6de129955894f93
pauls3 <paulstanik3@gmail.com>
2022-08-23 23:35:15 -0700

    updated test path

--- scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
+++ scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
@@ -6,4 +6,4 @@ python3 demo_floder.py \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
     --save_dir logs/railsem19_pretrained_cityscapes_0/no_eval_test_1 \
     --snapshot pretrained_models/railsem19_cityscapes_mapillary_epoch_252_mean-iou_0.68545.pth \
-    --demo_folder /home/stanik/rtis_lab/data/RailSem19/custom_split/test_images
+    --demo_folder input_data/stylegan2-ada-pytorch/test
