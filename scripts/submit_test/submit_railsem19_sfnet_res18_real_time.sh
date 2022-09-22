d9dbfadc67c81f8a58a5a8c31e5af7116cfc61c8
pauls3 <paulstanik3@gmail.com>
2022-09-21 17:17:07 -0700

    updated test path output

--- scripts/submit_test/submit_railsem19_sfnet_res18_real_time.sh
+++ scripts/submit_test/submit_railsem19_sfnet_res18_real_time.sh
@@ -9,5 +9,5 @@ python3 eval.py \
     --inference_mode whole \
     --scales 1 \
     --split test \
-    --ckpt_path logs/rs19_4000_pretrained_cityscapes_1/test_0 \
+    --ckpt_path logs/rs19_4000_pretrained_cityscapes_1/test_1 \
     --snapshot pretrained_models/best_epoch_1465_mean-iu_0.75616.pth
