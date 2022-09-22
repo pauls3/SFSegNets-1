c68a33e3ce94cd7ce4d918e0f53ed1c3897e26cb
pauls3 <paulstanik3@gmail.com>
2022-09-21 17:14:46 -0700

    updated test for new trained model

--- scripts/submit_test/submit_railsem19_sfnet_res18_real_time.sh
+++ scripts/submit_test/submit_railsem19_sfnet_res18_real_time.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # echo "Running inference on" ${1}
 # echo "Saving Results :" ${2}
+# --snapshot pretrained_models/rs19_4000_epoch_274_mean-iu_0.57821.pth
+
 python3 eval.py \
     --dataset railsem19 \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
@@ -8,4 +10,4 @@ python3 eval.py \
     --scales 1 \
     --split test \
     --ckpt_path logs/rs19_4000_pretrained_cityscapes_1/test_0 \
-    --snapshot pretrained_models/rs19_4000_epoch_274_mean-iu_0.57821.pth
+    --snapshot pretrained_models/best_epoch_1465_mean-iu_0.75616.pth
