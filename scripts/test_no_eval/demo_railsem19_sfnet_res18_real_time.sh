18d24520388467073cf0d9dd8eb9584b98c0970e
pauls3 <paulstanik3@gmail.com>
2022-09-21 17:44:26 -0700

    setting up inference on test set rs19

--- scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
+++ scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
@@ -5,6 +5,6 @@
 # --demo_folder /home/stanik/rtis_lab/data/RailSem19/custom_split/test_images
 python3 demo_floder.py \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
-    --save_dir logs/railsem19_pretrained_cityscapes_0/no_eval_test_2 \
-    --snapshot logs/rs19_trainVal_pretrained_cityscapes_2/test_model.pth \
-    --demo_folder input_data/stylegan2-ada-pytorch/test
+    --save_dir logs/railsem19_pretrained_cityscapes_0/no_eval_test_3 \
+    --snapshot logs/rs19_trainVal_pretrained_cityscapes_2/railsem19_cityscapes_mapillary_epoch_252_mean-iou_0.68545.pth \
+    --demo_folder /shared/rtis_lab/data/RailSem19/custom_split/test_images/
