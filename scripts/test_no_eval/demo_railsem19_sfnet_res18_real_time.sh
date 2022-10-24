1f6dde1ca5d7a8bc45475b23555358e47b4bf020
pauls3 <paulstanik3@gmail.com>
2022-10-24 15:06:43 -0700

    trying to inference with multiple gpus

--- scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
+++ scripts/test_no_eval/demo_railsem19_sfnet_res18_real_time.sh
@@ -3,7 +3,7 @@
 # echo "Saving Results :" ${2}
 # --snapshot pretrained_models/railsem19_cityscapes_mapillary_epoch_252_mean-iou_0.68545.pth \
 # --demo_folder /home/stanik/rtis_lab/data/RailSem19/custom_split/test_images
-python3 demo_floder.py \
+python3 -m torch.distributed.launch --nproc_per_node=4 demo_floder.py \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
     --save_dir logs/railsem19_pretrained_cityscapes_0/no_eval_test_3 \
     --snapshot pretrained_models/railsem19_cityscapes_mapillary_epoch_252_mean-iou_0.68545.pth \
