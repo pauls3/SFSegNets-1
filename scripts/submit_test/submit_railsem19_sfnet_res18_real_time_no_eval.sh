d1c63158ae0b27ec04a5bd7f378f5ab34bae2365
pauls3 <paulstanik3@gmail.com>
2022-10-25 02:23:45 -0700

    setting trunk; using pooling mode

--- scripts/submit_test/submit_railsem19_sfnet_res18_real_time_no_eval.sh
+++ scripts/submit_test/submit_railsem19_sfnet_res18_real_time_no_eval.sh
@@ -6,9 +6,11 @@
 python3 eval.py \
     --dataset railsem19 \
     --arch network.sfnet_resnet.DeepR18_SF_deeply \
-    --inference_mode whole \
+    --inference_mode pooling \
     --scales 1 \
+    --single_scale \
     --split test \
     --ckpt_path logs/pretrained_cityscapes_1_test_1 \
     --no_flip \
+    --trunk resnet18 \
     --snapshot logs/rs19_trainVal_pretrained_cityscapes_3/railsem19_SFnet_res18_lr_0.002/rail-network.sfnet_resnet.DeepR18_SF_deeply_apex_T_bblur_T_bs_mult_8_class_uniform_tile_1080_crop_size_1080_cv_0_dataset_railsem_lr_0.0025_ohem_T_PT_sbn/best_epoch_391_mean-iu_0.75268.pth
